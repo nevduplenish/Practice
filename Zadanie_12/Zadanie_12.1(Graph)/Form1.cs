7de9317012b07d0f2ed034f248b20a5bcf1b391b
nevduplenish <vip.nick600@mail.ru>
2021-12-21 21:22:40 +0300

    Practice
    Practice

---
+++ Zadanie_12/Zadanie_12.1(Graph)/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_12._1_Graph_
+{
+    public partial class Form1 : Form
+    {
+        Triangle triangle;
+        public class Triangle
+        {
+            public static Triangle triangle;
+            // Поля класса Triangle
+            public double a = 0;
+            public double b = 0;
+            public double c = 0;
+            public double p = 0;
+            public double S = 0;
+            // Конструктор класса с передаваемыми аргументами из textBox1,2,3
+            public Triangle(double a_, double b_, double c_)
+            {
+                // присваивание полям значений
+                a = a_;
+                b = b_;
+                c = c_;
+            }
+            public Triangle()
+            {
+            }
+            public double A
+            {
+                get { return a; }
+                set { a = value; }
+            }
+            public double B
+            {
+                get { return b; }
+                set { b = value; }
+            }
+            public double C
+            {
+                get { return c; }
+                set { c = value; }
+            }
+            public bool isTriangle
+            {
+                get
+                {
+                    if (a + b > c && a + c > b && b + c > a)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            public double this[double index]
+            {
+                get
+                {
+                    switch (index)
+                    {
+                        case 1:
+                            return a;
+                        case 2:
+                            return b;
+                        case 3:
+                            return c;
+                        default:
+                            throw new Exception("Error");
+                    }
+                }
+            }
+            public double P_()
+            {
+                p = (A + B + C) / 2;
+                return p;
+            }
+            public double AREA_()
+            {
+                double P = P_();
+                S = Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+                return S;
+            }
+            public string oper(double a, double b, double c)
+            {
+                a++;
+                b++;
+                c++;
+                string str = "";
+                str += Convert.ToString(a);
+                str += "\r\n";
+                str += Convert.ToString(b);
+                str += "\r\n";
+                str += Convert.ToString(c);
+                str += "\r\n";
+                return str;
+            }
+            public string oper2(double a, double b, double c)
+            {
+                a--;
+                b--;
+                c--;
+                string str = "";
+                str += Convert.ToString(a);
+                str += "\r\n";
+                str += Convert.ToString(b);
+                str += "\r\n";
+                str += Convert.ToString(c);
+                str += "\r\n";
+                return str;
+            }
+            public string oper3(double a, double b, double c, double s)
+            {
+
+                string str = "";
+                str += Convert.ToString(a * s);
+                str += "\r\n";
+                str += Convert.ToString(b * s);
+                str += "\r\n";
+                str += Convert.ToString(c * s);
+                str += "\r\n";
+                return str;
+            }
+            //преобразования типа Triangle в string (и наоборот)
+            public static explicit operator String(Triangle tr)
+            {
+                return tr.a + ", " + tr.b + ", " + tr.c;
+            }
+            public static explicit operator Triangle(string str)
+            {
+                string[] q = str.Split(new char[] { ',', ' ' },
+               StringSplitOptions.RemoveEmptyEntries);
+                Triangle tr = new Triangle();
+                tr.a = int.Parse(q[0]);
+                tr.b = int.Parse(q[1]);
+                tr.c = int.Parse(q[2]);
+                return tr;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a, b, c, s;
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            c = Convert.ToDouble(textBox3.Text);
+            s = Convert.ToDouble(textBox3.Text);
+            Triangle obj = new Triangle(a, b, c);
+            triangle = new Triangle(a, b, c);
+            textBox7.Text = Convert.ToString(triangle.isTriangle.ToString());
+            textBox5.Text = Convert.ToString(triangle.AREA_().ToString());
+            textBox4.Text = Convert.ToString(triangle.P_().ToString());
+            textBox8.Text = Convert.ToString(triangle.oper(a, b, c).ToString());
+            textBox9.Text = Convert.ToString(triangle.oper2(a, b, c).ToString());
+            textBox10.Text = Convert.ToString(triangle.oper3(a, b, c, s).ToString());
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Zadanie_12/Zadanie_12.1/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+namespace Zadanie_12._1
+{
+    class Program
+    {
+            public class Triangle
+        {
+            public static Triangle triangle;
+            
+            public double a = 0;
+            public double b = 0;
+            public double c = 0;
+            public double p = 0;
+            public double S = 0;
+            
+            public Triangle(double a_, double b_, double c_)
+            {
+                
+                a = a_;
+                b = b_;
+                c = c_;
+            }
+            public Triangle()
+            {
+            }
+            public double A
+            {
+                get { return a; }
+                set { a = value; }
+            }
+            public double B
+            {
+                get { return b; }
+                set { b = value; }
+            }
+            public double C
+            {
+                get { return c; }
+                set { c = value; }
+            }
+            public bool isTriangle
+            {
+                get
+                {
+                    if (a + b > c && a + c > b && b + c > a)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+            public double this[double index]
+            {
+                get
+                {
+                    switch (index)
+                    {
+                        case 1:
+                            return a;
+                        case 2:
+                            return b;
+                        case 3:
+                            return c;
+                        default:
+                            throw new Exception("Error");
+                    }
+                }
+            }
+            public double P_()
+            {
+                p = (A + B + C) / 2;
+                return p;
+            }
+            public double AREA_()
+            {
+                double P = P_();
+                S = Math.Sqrt(P * (P - A) * (P - B) * (P - C));
+                return S;
+            }
+            public string oper(double a, double b, double c)
+            {
+                a++;
+                b++;
+                c++;
+                string str = "";
+                str += Convert.ToString(a);
+                str += "\r\n";
+                str += Convert.ToString(b);
+                str += "\r\n";
+                str += Convert.ToString(c);
+                str += "\r\n";
+                return str;
+            }
+            public string oper2(double a, double b, double c)
+            {
+                a--;
+                b--;
+                c--;
+                string str = "";
+                str += Convert.ToString(a);
+                str += "\r\n";
+                str += Convert.ToString(b);
+                str += "\r\n";
+                str += Convert.ToString(c);
+                str += "\r\n";
+                return str;
+            }
+            public string oper3(double a, double b, double c)
+            {
+                Console.WriteLine("Введите скаляр(число на которое умножаем)");
+                double s = Convert.ToDouble(Console.ReadLine());
+                string str = "";
+                str += Convert.ToString(a * s);
+                str += "\r\n";
+                str += Convert.ToString(b * s);
+                str += "\r\n";
+                str += Convert.ToString(c * s);
+                str += "\r\n";
+                return str;
+            }
+            public static explicit operator String(Triangle tr)
+            {
+                return tr.a + ", " + tr.b + ", " + tr.c;
+            }
+            public static explicit operator Triangle(string str)
+            {
+                string[] q = str.Split(new char[] { ',', ' ' },
+               StringSplitOptions.RemoveEmptyEntries);
+                Triangle tr = new Triangle();
+                tr.a = int.Parse(q[0]);
+                tr.b = int.Parse(q[1]);
+                tr.c = int.Parse(q[2]);
+                return tr;
+            }
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите стороны треуголинка");
+                Console.WriteLine("Введите a");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите b");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите c");
+                double c = Convert.ToDouble(Console.ReadLine());
+                triangle = new Triangle(a, b, c);
+                Console.WriteLine("Существует ли треугольник");
+                Console.WriteLine(triangle.isTriangle.ToString());
+                Console.WriteLine("Площадь");
+                Console.WriteLine(triangle.AREA_().ToString());
+                Console.WriteLine("Периметр");
+                Console.WriteLine(triangle.P_().ToString());
+                Console.WriteLine("Выполнение команды ++");
+                Console.WriteLine(triangle.oper(a, b, c).ToString());
+                Console.WriteLine("Выполнение команды --");
+                Console.WriteLine(triangle.oper2(a, b, c).ToString());
+                Console.WriteLine("Выполнение команды *");
+                Console.WriteLine(triangle.oper3(a, b, c).ToString());
+            }
+        }
+    }
+    
+}
